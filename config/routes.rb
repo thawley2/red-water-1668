fcdc0fda7559f6aad817d83dd9e1449a49ef4711
Thomas Hawley <thawley22@turing.edu>
2023-04-18 13:48:18 -0600

    feat: Add resource routes for chefs/dishes#show

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :chefs, only: [] do
+    resources :dishes, only: :show, controller: 'chefs/dishes'
+  end
 end
