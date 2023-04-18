1e750710ba5e3e25aa481343d1e5e468b90f2a44
Thomas Hawley <thawley22@turing.edu>
2023-04-18 15:12:01 -0600

    feat: Add route for chefs and ingredients

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,8 @@ Rails.application.routes.draw do
   resources :dishes, only: :show do
     resources :dish_ingredients, only: :create
   end
+
+  resources :chefs, only: :show do
+    resources :ingredients, only: :index
+  end
 end
