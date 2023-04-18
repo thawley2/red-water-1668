716a9c87693824644b0ed190906e2d4d083f90c4
Thomas Hawley <thawley22@turing.edu>
2023-04-18 15:56:01 -0600

    test: Add method test for #most_popular_ingredients

--- spec/models/chef_spec.rb
+++ spec/models/chef_spec.rb
@@ -7,12 +7,14 @@ RSpec.describe Chef, type: :model do
 
     let!(:dish1) {chef1.dishes.create!(name: 'Tuna Salad', description: 'Yummy')}
     let!(:dish2) {chef1.dishes.create!(name: 'Grilled Steak', description: "Hope you aren't vegan")}
+    let!(:dish3) {chef1.dishes.create!(name: 'Hamburgers', description: "You can taste the charcoal")}
+    
 
     let!(:tuna) {Ingredient.create!(name: 'Tuna', calories: 156)}
     let!(:salt) {Ingredient.create!(name: 'Salt', calories: 15)}
     let!(:mayo) {Ingredient.create!(name: 'Mayo', calories: 1200)}
-
     let!(:steak) {Ingredient.create!(name: 'Steak', calories: 1500)}
+    let!(:noodle) {Ingredient.create!(name: 'Noodle', calories: 800)}
 
     let!(:dishingr1) {DishIngredient.create!(dish: dish1, ingredient: tuna)}
     let!(:dishingr2) {DishIngredient.create!(dish: dish1, ingredient: salt)}
@@ -20,6 +22,12 @@ RSpec.describe Chef, type: :model do
 
     let!(:dishingr4) {DishIngredient.create!(dish: dish2, ingredient: steak)}
     let!(:dishingr5) {DishIngredient.create!(dish: dish2, ingredient: salt)}
+    let!(:dishingr9) {DishIngredient.create!(dish: dish2, ingredient: mayo)}
+    let!(:dishingr10) {DishIngredient.create!(dish: dish2, ingredient: tuna)}
+    
+    let!(:dishingr6) {DishIngredient.create!(dish: dish3, ingredient: salt)}
+    let!(:dishingr7) {DishIngredient.create!(dish: dish3, ingredient: mayo)}
+    let!(:dishingr8) {DishIngredient.create!(dish: dish3, ingredient: salt)}
   describe "validations" do
     it {should validate_presence_of :name}
   end
@@ -33,6 +41,12 @@ RSpec.describe Chef, type: :model do
       it 'returns an array of unique ingredients the chef uses' do
         expect(chef1.all_ingredients).to eq(['Mayo', 'Salt', 'Steak', 'Tuna'])
         expect(chef2.all_ingredients).to eq([])
+      end
+    end
+
+    describe '#most_popular_ingredients' do
+      it 'returns the names of the top 3 ingredients' do
+        expect(chef1.most_popular_ingredients).to eq(['Salt', 'Mayo', 'Tuna'])
       end
     end
   end
