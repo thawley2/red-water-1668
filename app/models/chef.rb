c6c0ac57004a0f3bca3bd717432b80f9a6e58cdc
Thomas Hawley <thawley22@turing.edu>
2023-04-18 15:56:28 -0600

    feat: Add method for chef model #most_popular_ingredients

--- app/models/chef.rb
+++ app/models/chef.rb
@@ -6,5 +6,15 @@ class Chef < ApplicationRecord
 
   def all_ingredients
     ingredients.distinct.pluck(:name)
+    
+  end
+
+  def most_popular_ingredients
+    #I don't have time to implement this in the view but I wanted 
+    # to see if I could write a query that was close
+    ingredients.select("ingredients.*, count(dishes)")
+    .group(:id)
+    .order(count: :desc)
+    .limit(3).pluck(:name)
   end
 end
